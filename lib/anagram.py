b8cd0ffa64e5bfc7d5df5f9c62499a9ccf0de666
Tano Paul <tanopaul38@gmail.com>
2023-09-14 09:10:49 -0400

    completed anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, arr):
+        anagram_array = []
+        for arr_word in arr:
+            if sorted([*arr_word]) == sorted([*self.word]):
+               anagram_array.append(arr_word)
+            else:
+                return anagram_array
+        
+        return anagram_array
+
+
+listen = Anagram('listen')
+listen.match(['enlists', 'google', 'inlets', 'banana'])
